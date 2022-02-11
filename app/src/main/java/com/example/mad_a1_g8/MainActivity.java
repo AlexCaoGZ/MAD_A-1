1363adccae9167a67ee51431ed05eccce133eba0
Alex <82902418+AlexCaoGZ@users.noreply.github.com>
2022-02-10 21:26:09 -0500

    add the price display and the seek bar at the mainActivity

--- app/src/main/java/com/example/mad_a1_g8/MainActivity.java
+++ app/src/main/java/com/example/mad_a1_g8/MainActivity.java
@@ -6,11 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     String dest="na";
     String hotel="na";
+    int ticketPrice=0;
+    int hotelPrice=0;
+    int hotelNigths=0;
 
     Bundle bundle=new Bundle();
 
@@ -19,19 +25,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //resume datas
-        if(savedInstanceState != null)
-        {
-            dest = (String)savedInstanceState.getString("dest");
-            hotel = (String)savedInstanceState.getString("hotel");
-        }
-
         //recive the data that come from other activity
         Intent intent = getIntent();
         if(intent.hasExtra("bundle")) {
             bundle = intent.getBundleExtra("bundle");
             dest=bundle.getString("dest");
             hotel=bundle.getString("hotel");
+            ticketPrice=bundle.getInt("ticketPrice");
+            hotelPrice=bundle.getInt("hotelPrice");
         }
 
         if(!dest.equals("na")){
@@ -40,7 +41,32 @@ public class MainActivity extends AppCompatActivity {
         }
 
         TextView tvCity=findViewById(R.id.tvCity);
+        TextView tvTicketPrice=findViewById(R.id.tvTicketPrice);
         TextView tvHotle=findViewById(R.id.tvHotle);
+        TextView tvHotlePrice=findViewById(R.id.tvHotlePrice);
+        TextView tvHotleNights=findViewById(R.id.tvHotleNights);
+        SeekBar sbNights=(SeekBar)findViewById(R.id.nightsSeekBar);
+
+        sbNights.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                hotelNigths=i;
+                tvHotleNights.setText(String.valueOf(hotelNigths));
+                tvHotlePrice.setText(String.valueOf(hotelNigths*hotelPrice));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        tvTicketPrice.setText(String.valueOf(ticketPrice));
         tvCity.setText(dest);
         tvHotle.setText(hotel);
 
@@ -69,17 +95,11 @@ public class MainActivity extends AppCompatActivity {
                 Bundle extras=new Bundle();
                 extras.putString("Purpose",Purpose);
                 extras.putString("dest",dest);
+                extras.putInt("ticketPrice",ticketPrice);
                 intent.putExtra("bundle",extras);
                 startActivity(intent);
             }
         });
     }
 
-    @Override
-    protected void onSaveInstanceState(Bundle outState)
-    {
-        super.onSaveInstanceState(outState);
-        outState.putString("dest", dest);
-        outState.putString("hotel", hotel);
-    }
 }
--- app/src/main/java/com/example/mad_a1_g8/selectPage.java
+++ app/src/main/java/com/example/mad_a1_g8/selectPage.java
@@ -11,8 +11,15 @@ import android.widget.ListView;
 
 public class selectPage extends AppCompatActivity {
     private ListView lv;
-    String[] cities={"Toronto    114KM","Vancouver   4,171km"};
+    String[] cities={"Toronto    114Km  $11","Vancouver   4,171Km   $417"};
+    int[] ticketPrice={11,417};
+
     String[] hotelTOR={"TORhotelA","TORhotelB"};
+    String[] hotelTORwithPrice={"TORhotelA   $20/Night","TORhotelB  $30/Night"};
+    int[] hotelTORPrice={20,30};
+
+    String[] hotelVANwithPrice={"VANhotelA   $15/Night","VANhotelB  $25/Night"};
+    int[] hotelVANPrice={15,25};
     String[] hotelVAN={"VANhotelA","VANhotelB"};
 
     Bundle bundle=new Bundle();
@@ -43,6 +50,7 @@ public class selectPage extends AppCompatActivity {
                 public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                     Intent intent = new Intent(selectPage.this, MainActivity.class);
                     bundle.putString("dest",cities[i]);
+                    bundle.putInt("ticketPrice",ticketPrice[i]);
                     intent.putExtra("bundle", bundle);
                     startActivity(intent);
                 }
@@ -51,7 +59,7 @@ public class selectPage extends AppCompatActivity {
             if (dest.equals("na")) {
                 //dest did not selected, cant show the hotels
             } else if (dest.equals("Toronto")) {
-                ArrayAdapter<String> adapter = new ArrayAdapter<String>(selectPage.this, android.R.layout.simple_list_item_single_choice, hotelTOR);
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(selectPage.this, android.R.layout.simple_list_item_single_choice, hotelTORwithPrice);
                 lv.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
                 lv.setAdapter(adapter);
 
@@ -60,6 +68,7 @@ public class selectPage extends AppCompatActivity {
                     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                         Intent intent = new Intent(selectPage.this, MainActivity.class);
                         bundle.putString("hotel",hotelTOR[i]);
+                        bundle.putInt("hotelPrice",hotelTORPrice[i]);
                         intent.putExtra("bundle", bundle);
                         startActivity(intent);
                     }
@@ -74,6 +83,7 @@ public class selectPage extends AppCompatActivity {
                     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                         Intent intent = new Intent(selectPage.this, MainActivity.class);
                         bundle.putString("hotel",hotelVAN[i]);
+                        bundle.putInt("hotelPrice",hotelVANPrice[i]);
                         intent.putExtra("bundle", bundle);
                         startActivity(intent);
                     }
